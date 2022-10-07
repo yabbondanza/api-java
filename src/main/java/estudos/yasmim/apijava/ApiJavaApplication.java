7a62d8e3419bb920d64ce0a33a027a49dc6f369b
Yasmim <yasmimabbondanza@gmail.com>
2022-10-07 13:24:41 -0300

    Inicialização do projeto

---
+++ src/main/java/estudos/yasmim/apijava/ApiJavaApplication.java
@@ -0,0 +1,13 @@
+package estudos.yasmim.apijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudos/yasmim/apijava/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.yasmim.apijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
